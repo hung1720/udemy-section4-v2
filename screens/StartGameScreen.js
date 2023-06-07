6eff17d23abe16c09b6368d738d27e0befcd53a3
hung1720 <hungtdqse140575@fpt.edu.vn>
2023-06-07 22:15:33 +0700

    52

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
     return (
         <View style={styles.inputContainer}>
-            <TextInput />
+            <TextInput 
+                style={styles.numberInput} 
+                maxLength={2}
+                keyboardType="number-pad"
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
@@ -14,9 +20,26 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flex: 1,
         marginTop: 100,
         padding: 16,
-        backgroundColor: '#72063c',
+        marginHorizontal: 24,
+        backgroundColor: '#4e0329',
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center'
     }
 })
